63c94d40d20b935aa0f1b9840d07694d8d6c9f2c
yashaswinij08 <jyashaswini288@gmail.com>
2025-08-25 23:36:38 +0530

    Add to cart logic implemented

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -5,6 +5,8 @@ import { assets } from '../../assets/assets'
 const FoodItem = ({id,name,price,description,image}) => {
 
   const [itemCount,setItemCount]=useState(0)
+
+  
   return (
     <div className='food-item'>
         <div className="food-item-img-container"> 
